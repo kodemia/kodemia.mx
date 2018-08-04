e152a104d0ed98e59a0b650081be70d4bf86426a
Joe Cohen <joecohenr@gmail.com>
2018-08-04 03:26:26 -0500

    remove is dev check from ga

--- components/analytics/ga/index.js
+++ components/analytics/ga/index.js
@@ -5,13 +5,10 @@ function isLocal() {
   return location.hostname === 'localhost'
 }
 
-function isDev() {
-  return process.env.NODE_ENV !== 'production'
-}
 export default (code, { router }) => Page => {
   class WithAnalytics extends Component {
     componentDidMount() {
-      const shouldTrack = isLocal() || isDev()
+      const shouldTrack = isLocal()
       if (!shouldTrack) return
       analytics.init(code)
       analytics.pageview()

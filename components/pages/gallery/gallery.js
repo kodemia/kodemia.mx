b1d442e7c43c4d379beacd00f31e800ebe83aedd
Samuel Olvera <jsamline@gmail.com>
2018-08-27 14:32:34 -0500

    added mentors thumbs and gallery

--- components/pages/gallery/gallery.js
+++ components/pages/gallery/gallery.js
@@ -1,11 +1,27 @@
 // Package
 import React, { Component } from 'react'
-// import asset from 'next/asset'
-
-// Ours
-import ImagesList from '../../../config/gallery'
+import asset from 'next/asset'
 
 class Gallery extends Component {
+  createItems = () => {
+    let Item = []
+
+    for (let i = 0; i < 12; i++) {
+      Item.push(
+        <div className="s:scol-6 m:scol-4 x:pad-all-5">
+          <div className="gallery-item">
+            <img
+              className="img-liquid"
+              src={asset('/img/galleries/gallery/foto-' + (i + 1) + '.jpg')}
+              alt="Kodemia"
+            />
+          </div>
+        </div>
+      )
+    }
+
+    return Item
+  }
   render() {
     return (
       <section
@@ -16,21 +32,7 @@ class Gallery extends Component {
           <div className="line">
             <div className="x:scol-12">
               <div className="gallery">
-                <div className="line">
-                  {ImagesList.map((image, i) => (
-                    <div className="x:scol-6 m:scol-4 x:pad-all-5" key={i}>
-                      <a href={image} data-fancybox="gallery">
-                        <div className="gallery-item">
-                          <img
-                            className="img-liquid"
-                            src={image}
-                            alt="Kodemia"
-                          />
-                        </div>
-                      </a>
-                    </div>
-                  ))}
-                </div>
+                <div className="line">{this.createItems()}</div>
               </div>
             </div>
           </div>

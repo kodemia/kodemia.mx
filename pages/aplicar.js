ded3df0516e5a32159336a421be933d78457b3ac
samline <jsamline@gmail.com>
2018-11-13 19:37:15 -0600

    commented gtag

--- pages/aplicar.js
+++ pages/aplicar.js
@@ -160,21 +160,21 @@ export default withFormik({
     )
   }),
   async handleSubmit(values, { resetForm, setSubmitting }) {
-    let dataLayer = window.dataLayer || []
+    /*let dataLayer = window.dataLayer || []
     function gtag() {
       dataLayer.push(arguments)
-    }
+    }*/
 
     try {
       const res = await axios.post('/.netlify/functions/apply', values)
 
       if (res && res.data.success) {
         resetForm()
-        gtag('js', new Date())
+        /*gtag('js', new Date())
         gtag('config', 'AW-781067354')
         gtag('event', 'conversion', {
           send_to: 'AW-781067354/w_HcCKzxmI0BENrIuPQC'
-        })
+        })*/
       }
 
       Router.push('/thankyou')

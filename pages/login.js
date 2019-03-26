6fbed5f282bd700425ccbd81c9c03bb528739f80
Manuel Eljuri <manueleljuri@gmail.com>
2019-03-26 02:09:15 -0600

    login  primera version

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 // Packages
 import Layout from '../components/layout'
-import Logo from '../static/img/logos/logo-h-for-white.png'
+//import Logo from '../static/img/logos/logo-h-for-white.png'
 //import ReactPlayer from 'react-player'
 
 //const playbackId = 'gLdt7FXa43r1xPFkHS01KG00j400ok42WWS'
@@ -8,19 +8,24 @@ import Logo from '../static/img/logos/logo-h-for-white.png'
 
 const Live = () => (
   <Layout title="Live :: Kodemia">
-    <div className="container">
+    <div className="login-container">
       <div className="login-card">
-        <header>
-          <div className="logo">
+        <header className="login-header">
+          {/*<div className="logo">
             <img src={Logo} alt="" />
-          </div>
+            </div>*/}
+          <h1 className="login-title">Bienvenid@</h1>
         </header>
-        <div className="card-body">
-          <input placeholder="Email" type="email" />
-          <input type="password" placeholder="Password" />
+        <div className="login-body">
+          <input className="login-input" placeholder="Email" type="email" />
+          <input
+            className="login-input"
+            placeholder="Password"
+            type="password"
+          />
         </div>
         <footer>
-          <button>Login</button>
+          <button className="login-button">Login</button>
         </footer>
       </div>
     </div>

a4dcc3c7b7a174f9b11c67d5d1e09fe5a1e547ed
charles <charlesipn@gmail.com>
2019-03-27 14:59:38 -0600

    streaming production

--- pages/live.js
+++ pages/live.js
@@ -2,13 +2,13 @@
 import Layout from '../components/layout'
 import ReactPlayer from 'react-player'
 
-const playbackId = 'gLdt7FXa43r1xPFkHS01KG00j400ok42WWS'
+const playbackId = 'D4Sd017h8DWcy7QYtKt01N00VV4Hi01SESOV'
 const url = 'https://stream.mux.com/' + playbackId + '.m3u8'
 
 const Live = () => (
   <Layout title="Live :: Kodemia">
     <div className="player">
-      <h2>Logica y pensamiento estructurado</h2>
+      <h2>HTML y CSS</h2>
       <ReactPlayer
         fileConfig={{
           forceHLS: true

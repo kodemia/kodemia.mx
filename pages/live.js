773b8443b1dccbf6dde9f7806e82341d78fc0ed5
charles <charlesipn@gmail.com>
2019-03-28 15:06:27 -0600

    login redirect to live

--- pages/live.js
+++ pages/live.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react'
+import Router from 'next/router'
+
 // Packages
 import Layout from '../components/layout'
 import ReactPlayer from 'react-player'
@@ -5,22 +8,31 @@ import ReactPlayer from 'react-player'
 const playbackId = 'D4Sd017h8DWcy7QYtKt01N00VV4Hi01SESOV'
 const url = 'https://stream.mux.com/' + playbackId + '.m3u8'
 
-const Live = () => (
-  <Layout title="Live :: Kodemia">
-    <div className="player">
-      <h2>HTML y CSS</h2>
-      <ReactPlayer
-        config={{
-          file: {
-            forceHLS: true
-          }
-        }}
-        url={url}
-        playing
-        controls
-      />
-    </div>
-  </Layout>
-)
+class Live extends Component {
+  componentDidMount() {
+    const token = sessionStorage.getItem('token')
+    // TODO: validate if the token is valid
+    if (!token) Router.replace('/login')
+  }
+
+  render() {
+    return (
+      <Layout title="Live :: Kodemia">
+        <div className="player">
+          <h2>HTML y CSS</h2>
+          <ReactPlayer
+            config={{
+              file: {
+                forceHLS: true
+              }
+            }}
+            url={url}
+            controls
+          />
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default Live
--- pages/login.js
+++ pages/login.js
@@ -1,35 +1,85 @@
-// Packages
+import React, { Component } from 'react'
+import Router from 'next/router'
+
+// Tools
+import { login } from '../lib/api'
+
+// Components
 import Layout from '../components/layout'
-//import Logo from '../static/img/logos/logo-h-for-white.png'
-//import ReactPlayer from 'react-player'
 
-//const playbackId = 'gLdt7FXa43r1xPFkHS01KG00j400ok42WWS'
-// const url = 'https://stream.mux.com/' + playbackId + '.m3u8'
+export default class Live extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      error: false
+    }
+  }
 
-const Live = () => (
-  <Layout title="Live :: Kodemia">
-    <div className="login-container">
-      <div className="login-card">
-        <header className="login-header">
-          {/*<div className="logo">
-            <img src={Logo} alt="" />
-            </div>*/}
-          <h1 className="login-title">Bienvenid@</h1>
-        </header>
-        <div className="login-body">
-          <input className="login-input" placeholder="Email" type="email" />
-          <input
-            className="login-input"
-            placeholder="Password"
-            type="password"
-          />
+  setError() {
+    this.setState({ error: true })
+    setTimeout(() => this.setState({ error: false }), 2000)
+  }
+
+  signIn = () => {
+    const { email, password } = this.state
+    if (!email || !email) return this.setError()
+
+    login(email, password)
+      .then(token => {
+        sessionStorage.setItem('token', token)
+        Router.replace('/live')
+      })
+      .catch(() => this.setError())
+  }
+
+  componentDidMount() {
+    const token = sessionStorage.getItem('token')
+    if (token) Router.replace('/live')
+  }
+
+  render() {
+    const { email, password, error } = this.state
+    return (
+      <Layout title="Live :: Kodemia">
+        <div className="login-container">
+          <div className="login-card">
+            <header className="login-header">
+              <h1 className="login-title">Bienvenid@</h1>
+            </header>
+            <div className="login-body">
+              <input
+                className="login-input"
+                placeholder="Email"
+                type="email"
+                value={email}
+                onChange={({ target }) =>
+                  this.setState({ email: target.value })
+                }
+              />
+              <input
+                className="login-input"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={({ target }) =>
+                  this.setState({ password: target.value })
+                }
+              />
+            </div>
+            <footer>
+              <button
+                className={`login-button ${error ? 'has-error' : ''} `}
+                onClick={this.signIn}
+                disabled={error}
+              >
+                {error ? 'Datos invalidos' : 'Ingresar'}
+              </button>
+            </footer>
+          </div>
         </div>
-        <footer>
-          <button className="login-button">Login</button>
-        </footer>
-      </div>
-    </div>
-  </Layout>
-)
-
-export default Live
+      </Layout>
+    )
+  }
+}
